01c8e3b05b442855d87e7cec2eb666d95c498915
Mosa6 <tikamongi@gmail.com>
2022-04-13 00:46:49 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, onry: [:show, :edit]
   get "/homes/about" => "homes#about", as: "about"
 end
